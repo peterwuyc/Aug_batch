5c6842fe0e267752ab8c29a5bf3b94d01f5e54c3
unknown <jingjing.liu@yahoo.com>
2021-08-10 13:11:04 -0500

    update

--- hw8.6/Controller.java
+++ hw8.6/Controller.java
@@ -1,4 +1,59 @@
 package Spring;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Controller {
+    public static void main(String[] args) throws NullPointerException{
+        static Map<Integer,String> map= new HashMap<Integer,String>();
+        public static void main(String[] args) throws NullPointerException{
+            while (true){
+                Scanner sc = new Scanner(System.in);
+                Service serv = new ServiceImpl();
+                if(sc.hasNext()){
+                    String input=sc.next();
+                    String[] s= input.split("[,.]+");
+
+                    switch(s[0].toUpperCase()){
+                        case "ADD" : case "UPDATE" :
+                            try{
+                                serv.updateElementByKey(Integer.valueOf(s[1]), s[2]);
+                            }
+                            catch (Exception e){
+                                System.out.println(e);
+                            }
+                            break;
+
+                        case "DELETE" :
+                            try {
+                                serv.deleteElementByKey(Integer.valueOf(s[1]));
+                            }
+                            catch (Exception e){
+                                System.out.println(e);
+                            }
+                            break;
+
+                        case "SELECT" :
+                            try{
+                                if(s[1].toUpperCase()== "ALL") {
+                                    serv.getAllElements();
+                                }
+                                else{
+                                    System.out.println(s[1] + "," + serv.getElementByKey(Integer.valueOf(s[1])));
+                                }
+                            }
+                            catch (Exception e){
+                                System.out.println(e);
+                            }
+                            break;
+                        default :
+                            System.out.println("Invalid Command");
+                    }
+                }
+            }
+
+
+
+        }
 }
--- hw8.6/ServiceImpl.java
+++ hw8.6/ServiceImpl.java
@@ -1,5 +1,36 @@
 package Spring;
 
-public class ServiceImpl implements Service {
+import java.util.List;
 
+public class ServiceImpl implements Service {
+    Dao dao = new DaoImplement();
+
+    public ServiceImpl(){
+
+    }
+
+    @Override
+    public void updateElementByKey(Integer key, String value) {
+        dao.updateElementByKey(Integer key, String value);
+    }
+
+    @Override
+    public void deleteElementByKey(Integer key) {
+         dao.deleteElementByKey();
+    }
+
+    @Override
+    public String getElementByKey(Integer key) {
+        return dao.getElementByKey( Integer key);
+    }
+
+    @Override
+    public void createElement(Integer key, String value) {
+        dao.createElement(Integer key, String value);
+    }
+
+    @Override
+    public List getAllElements() {
+        return dao.getAllElements();
+    }
 }
