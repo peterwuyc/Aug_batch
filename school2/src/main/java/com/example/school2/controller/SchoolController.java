d31a402ec8678317759170948e3b0fc86a77f341
Chongxuan <ycxkzez@gmail.com>
2021-08-31 17:09:27 -0400

    school2

---
+++ school2/src/main/java/com/example/school2/controller/SchoolController.java
@@ -0,0 +1,16 @@
+package com.example.school2.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SchoolController {
+
+    @Value("${server.port}")
+    private String serverPort;
+    @GetMapping("/school")
+    public String getSchool(){
+        return "school"+serverPort;
+    }
+}
